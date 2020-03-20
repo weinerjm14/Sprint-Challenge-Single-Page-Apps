7d344524a4740d7ca46fb2b3398b6c7ad3d47b1e
Jennifer Weiner <58009503+weinerjm14@users.noreply.github.com>
2020-03-20 19:46:43 -0400

    retrievv data and set characters from response

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Header from "./components/Header.js";
-
+import CharacterList from "./components/CharacterList";
 export default function App() {
   return (
     <main data-testid='app'>
       <Header />
+      <CharacterList></CharacterList>
     </main>
   );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,46 @@
 import React, { useEffect, useState } from "react";
 
+import axios from "axios";
+
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
-
+  const [characters, setCharacters] = useState([]);
+  ;
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    const getChars = () => {axios
+    .get('https://rickandmortyapi.com/api/character')
+    .then(response => {
+      // console.log("characters respons", response.data.characters);
+      setCharacters(response.data.results);
+      
+    })
+    .catch(error => {
+      console.log(error);
+    });}
+    getChars();
   }, []);
-
+  //example from react wars to comapare to
+  // useEffect(() => {
+  //   const getMovies = () => {
+  //     axios
+  //       .get('http://localhost:5000/api/movies')
+  //       .then(response => {
+  //         setMovies(response.data);
+  //       })
+  //       .catch(error => {
+  //         console.error('Server Error', error);
+  //       });
+  //   }
+    
+  //   getMovies();
+  // }, []);
+  // console.log('character list', characters)
   return (
     <section className="character-list">
       <h2>TODO: `array.map()` over your state here!</h2>
+      
     </section>
   );
 }
