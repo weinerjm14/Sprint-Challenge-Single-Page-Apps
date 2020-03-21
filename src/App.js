119b42c0f3a5c96f95a3dd2558a5126e6c29501a
Jennifer Weiner <58009503+weinerjm14@users.noreply.github.com>
2020-03-20 21:56:35 -0400

    Work on Nav
    Computer issues

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
-import Header from "./components/Header.js";
+// import Header from "./components/Header.js";
 // import CharacterList from "./components/CharacterList";
 // import CharacterCard from "./components/CharacterCard";
+
+import WelcomePage from "./components/WelcomePage";
 
 export default function App() {
   return (
     <main data-testid='app'>
-      <Header />
+      <WelcomePage />
       
     </main>
   );
